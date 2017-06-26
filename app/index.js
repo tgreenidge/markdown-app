b66a078a221ff2d85622973978887d0ee4dd2cf7
Tisha Greenidge <tishagreenidge@ITs-MacBook-Pro.local>
2017-06-26 17:50:53 -0400

    add react and react-dom

---
+++ app/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+class App extends React.Component {
+
+	render() {
+		return (
+
+			)
+	}
+}
+
+ReactDOM.render()
